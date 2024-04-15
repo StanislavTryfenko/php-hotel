546506aec471917dad4d9e6a71ecbf722f3afbd8
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-04-15 15:46:11 +0200

    tabella con bs5

--- index.php
+++ index.php
@@ -51,6 +51,9 @@ $park_filter = $_GET['parking_filter'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <script src="https://kit.fontawesome.com/4c899c3a4a.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -60,17 +63,39 @@ $park_filter = $_GET['parking_filter'];
         <input type="checkbox" name="parking_filter">
         <button type="submit">Invia</button>
     </form>
-
-    <?php
-    foreach ($hotels as $hotel) {
-        if ($park_filter && $hotel['parking']) {
-            echo $hotel['name'] . ' ' . $hotel['vote'] . ' ' . $hotel['distance_to_center'] . '<br>';
-        } elseif (!$park_filter) {
-            echo $hotel['name'] . ' ' . $hotel['vote'] . ' ' . $hotel['distance_to_center'] . '<br>';
-        }
-    }
-    ?>
-
+    <div class="container mt-5">
+        <table class="table col-4">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($hotels as $i => $hotel) {
+                    if ($park_filter && $hotel['parking']) {
+                        echo '<tr>';
+                        echo '<th scope="row">' . $i . '</th>';
+                        echo '<td>' . $hotel['name'] . '</td>';
+                        echo '<td>' . $hotel['vote'] . '</td>';
+                        echo '<td>' . $hotel['distance_to_center'] . '</td>';
+                        echo '</tr>';
+                    } elseif (!$park_filter) {
+                        echo '<tr>';
+                        echo '<th scope="row">' . $i . '</th>';
+                        echo '<td>' . $hotel['name'] . '</td>';
+                        echo '<td>' . $hotel['vote'] . '</td>';
+                        echo '<td>' . $hotel['distance_to_center'] . '</td>';
+                        echo '</tr>';
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
